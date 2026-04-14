6c1a51e154abd45aa11f2eb10f675e4f4d19c31e
yazsosa01@gmail.com <yazsosa01@gmail.com>
2026-04-14 18:37:26 -0300

    Ejercicio 1- variables y operaciones

--- Ejercicios/C03_JavaScript/Ej1- variables y operaciones/app.js
+++ Ejercicios/C03_JavaScript/Ej1- variables y operaciones/app.js
@@ -1,12 +1,12 @@
-// Variables
+
 const nombre = "Yazmin";
 const edad = 21;
 const materia = "Desarrollo de Software";
 
-// Template literal
+
 console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
 
-// Contador
+
 let contador = 0;
 contador ++;
 contador ++;
